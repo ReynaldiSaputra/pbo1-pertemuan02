e65a3ed2c3e310981369e6f094da0b3a0eac65e2
ReynaldiSaputra <reynaldi.saputrafti18@gmail.com>
2020-04-11 19:50:11 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa ===============");
+  System.out.println("NIDN  : 18630464 ");
+  System.out.println("Nama  : Reynaldi Saputra ");
+  System.out.println("=================================");
+  System.out.println();
+ }
+}
